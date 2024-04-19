1181939dc6f6662ceff2c49ca7096b91751a3852
@Maival1 <maival.cimba@gmail.com>
2024-04-19 16:18:46 +0200

    feat(add container and header)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { Layout } from './components/Layout'
 export function App () {
   return (
-    <div>
-      <h1>MSlizh</h1>
-    </div>
+   <>
+    <Layout />
+   </>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+export function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__container">
+          <h1 className='header__logo'>MSlizh</h1>
+
+          <div className='header__nav'>
+            <a href="#" className='header__link'>Home</a>
+            <a href="#" className='header__link'>About</a>
+            <a href="#" className='header__link'>Projects</a>
+            <a href="#" className='header__link'>Contact</a>
+          </div>
+        </div>
+
+      </div>
+    </header>
+  )
+}
